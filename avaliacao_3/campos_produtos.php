a50b6f2b5e4ffd6e37d7c1ec08182b63bee1c7da
MatheusFreitas <matheus.freitas47@yahoo.com.br>
2014-01-13 18:11:10 -0200

    Create campos_produtos.php

---
+++ avaliacao_3/campos_produtos.php
@@ -0,0 +1,34 @@
+<label>Nome Dos Produtos: </label>
+<input type="text" name="nome" />
+<br/>
+<input type="radio" name="sexo" value="verde" checked="checked" />
+<label>verde</label>
+<input type="radio" name="sexo" value="rosa" checked="checked" />
+<label>rosa</label>
+<input type="radio" name="sexo" value="amarelo" checked="checked" />
+<label>amarelo</label>
+<input type="radio" name="sexo" value="vermelho" checked="checked" />
+<label>vermelho</label>
+<br/>
+<input type="checkbox" name="aceito" value="sim" />
+<label>Produto Novo</label>
+<br/>
+<label>Produtos</label>
+<select name="Produtos">
+    <option>Perfume</option>
+    <option>Joias</option>
+    <option>Creme de Pele</option>
+    <option>Roupas</option>
+</select>
+<br/>
+<label>Data De Entrega</label>
+<select name="Data De Entrega">
+    <option>5dias</option>
+    <option>10dias</option>
+    <option>15dias</option>
+</select>
+<br/>
+<label>Observaçãoes</label>
+<br/>
+<textarea name="observacoes"></textarea>
+<br/>
